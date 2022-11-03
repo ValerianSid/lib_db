cd883416d1cd7b1c7b5cf9a2f1fd9db5c1b781f1
12345 <drus@mail.ru>
2022-11-03 18:36:29 +0300

    dz

--- lib_db.sql
+++ lib_db.sql
@@ -234,3 +234,26 @@ select a.last_name , p."name"  from author a , poem p where a.last_name = 'Ки�
 select * from poem p where p.a_id in (select a.id from author a where a.last_name  = 'Кинг');
 
 select * from poem p where p.book_id in (select b.id from book b where b.producer  ='prod7');
+
+select * from poem p where p.date < '01.01.1951' and p.date > '01.01.1901';
+
+select * from poem p where p.book_id in (select b.id from book b where b.producer = 'prod3');
+
+
+
+select * from author a where a.id in (select a_id  from book b where b.shelf_id  in (
+select s.id from shelf s where s.book_case_id in (
+select bc.id from book_case bc where bc.archive_id = 2))); 
+
+
+select *from poem p where p.date >'01.01.1801' and p.date < '01.01.1900' and p.book_id in(
+select b.id  from book b where b.shelf_id  in (
+select s.id from shelf s where s.book_case_id in (
+select bc.id from book_case bc where bc.archive_id = 1)));
+
+select * from poem p where p.a_id in(
+select a.id from author a where a.birthdate > '01.01.1801' and a.birthdate <'01.01.1900')
+and p.book_id in(
+select b.id  from book b where b.shelf_id  in (
+select s.id from shelf s where s.book_case_id in (
+select bc.id from book_case bc where bc.archive_id = 1)));
